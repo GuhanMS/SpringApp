5f7601efd5c72712fea87b423c952a0a55a1979c
GuhanMS <guhanmsguga@gmail.com>
2022-06-28 13:00:12 +0530

    first commit

---
+++ FacultyPortal/src/main/java/com/FacultyPortal/FacultyPortalApplication.java
@@ -0,0 +1,13 @@
+package com.FacultyPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacultyPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacultyPortalApplication.class, args);
+	}
+
+}
---
+++ FacultyPortal/src/test/java/com/FacultyPortal/FacultyPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FacultyPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacultyPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
